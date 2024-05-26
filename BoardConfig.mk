45d9968e3456740bae58b551e6430d9318168236
Emmanuel <alzadidine33@gmail.com>
2024-05-26 16:56:49 +0000

    OP4BFB: Add architecture and bootloader flags
    Signed-off-by: Emmanuel <alzadidine33@gmail.com>
    Signed-off-by: KAGA-KOKO <kotakandis185@gmail.com>

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -11,4 +11,23 @@ TARGET_SYSTEM_PROP += $(DEVICE_PATH)/system.prop
 DEVICE_MANIFEST_FILE := $(DEVICE_PATH)/configs/manifests/manifest.xml
 DEVICE_MATRIX_FILE := $(DEVICE_PATH)/configs/manifests/compatibility_matrix.xml
 
+# Architecture
+TARGET_ARCH := arm64
+TARGET_ARCH_VARIANT := armv8-a
+TARGET_CPU_ABI := arm64-v8a
+
+TARGET_CPU_VARIANT := generic
+TARGET_CPU_VARIANT_RUNTIME := cortex-a53
+TARGET_2ND_CPU_VARIANT := generic
+TARGET_2ND_CPU_VARIANT_RUNTIME := cortex-a53
+
+TARGET_2ND_ARCH := arm
+TARGET_2ND_ARCH_VARIANT := armv8-a
+TARGET_2ND_CPU_ABI := armeabi-v7a
+TARGET_2ND_CPU_ABI2 := armeabi
+
+# Bootloader
+TARGET_BOOTLOADER_BOARD_NAME := oppo6765_19451
+TARGET_NO_BOOTLOADER := true
+
 -include vendor/oppo/CPH2083/BoardConfigVendor.mk
