0d7cc815915ffcbf48622cf48aafb2f2a723d1ca
Emmanuel <alzadidine33@gmail.com>
2024-06-30 08:09:31 +0000

    OP4BFB: Import rootdir scripts from stock
    Signed-off-by: Emmanuel <alzadidine33@gmail.com>
    Signed-off-by: KAGA-KOKO <kotakandis185@gmail.com>

--- device.mk
+++ device.mk
@@ -74,5 +74,26 @@ PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/wifi/wpa_supplicant.conf:$(TARGET_COPY_OUT_VENDOR)/etc/wifi/wpa_supplicant.conf \
     $(LOCAL_PATH)/configs/wifi/wpa_supplicant_overlay.conf:$(TARGET_COPY_OUT_VENDOR)/etc/wifi/wpa_supplicant_overlay.conf \
 
+# Ramdisk
+PRODUCT_PACKAGES += \
+    factory_init.connectivity.rc \
+    factory_init.project.rc \
+    factory_init.rc \
+    init.aee.rc \
+    init.ago.rc \
+    init.connectivity.rc \
+    init.modem.rc \
+    init.mt6765.rc \
+    init.mt6765.usb.rc \
+    init.project.rc \
+    init.sensor_1_0.rc \
+    meta_init.connectivity.rc \
+    meta_init.modem.rc \
+    meta_init.project.rc \
+    meta_init.rc \
+    multi_init.rc \
+    fstab.mt6765 \
+    ueventd.mt6765.rc
+
 # Inherit vendor
-$(call inherit-product, vendor/oppo/CPH2083/CPH2083-vendor.mk)
+$(call inherit-product, vendor/oppo/CPH2083/CPH2083-vendor.mk
