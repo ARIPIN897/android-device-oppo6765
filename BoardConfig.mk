e8226b4b13ad28b0ec2b529233d2fc2b1030e19c
Emmanuel <alzadidine33@gmail.com>
2024-07-27 16:26:23 +0000

    OP4BFB: Import system properties from stock
    Signed-off-by: Emmanuel <alzadidine33@gmail.com>
    Signed-off-by: KAGA-KOKO <kotakandis185@gmail.com>

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -4,6 +4,9 @@ BOARD_VENDOR := oppo
 # Security patch level
 VENDOR_SECURITY_PATCH := 2020-05-05
 
+# Properties
+TARGET_SYSTEM_PROP += $(DEVICE_PATH)/system.prop
+
 # VINTF
 DEVICE_MANIFEST_FILE := $(DEVICE_PATH)/configs/manifests/manifest.xml
 DEVICE_MATRIX_FILE := $(DEVICE_PATH)/configs/manifests/compatibility_matrix.xml
