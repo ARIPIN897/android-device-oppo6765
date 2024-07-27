d50eed19579bd466cb2ec06a1ae03f120ce52813
Emmanuel <alzadidine33@gmail.com>
2024-07-27 14:53:51 +0000

    OP4BFB: Import device manifests from stock * from CPH2083PUEX_11.A.67_0670_202201182155
    Signed-off-by: Emmanuel <alzadidine33@gmail.com>
    Signed-off-by: KAGA-KOKO <kotakandis185@gmail.com>

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -4,4 +4,8 @@ BOARD_VENDOR := oppo
 # Security patch level
 VENDOR_SECURITY_PATCH := 2020-05-05
 
+# VINTF
+DEVICE_MANIFEST_FILE := $(DEVICE_PATH)/configs/manifests/manifest.xml
+DEVICE_MATRIX_FILE := $(DEVICE_PATH)/configs/manifests/compatibility_matrix.xml
+
 -include vendor/oppo/CPH2083/BoardConfigVendor.mk
