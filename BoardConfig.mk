unasir008/android_device_samsung_a12-port

RMX2185: Explicitly Define to Compile Kernel with Clang 9 * This is how MediaTek prefers in alps & so do OEM's , we will follow the same.
Signed-off-by: techyminati <sinha.aryan03@gmail.com>

BoardConfig.mk
@@ -83,7 +83,7 @@ TARGET_KERNEL_CROSS_COMPILE_PREFIX := $(shell pwd)/prebuilts/gcc/linux-x86/aarch
 TARGET_KERNEL_CONFIG := karashi_defconfig
 TARGET_KERNEL_SOURCE := kernel/realme/RMX2185
 TARGET_KERNEL_CLANG_COMPILE := true
-
+TARGET_KERNEL_CLANG_VERSION := r353983c1
 BOARD_MKBOOTIMG_ARGS += --base $(BOARD_KERNEL_BASE)
 BOARD_MKBOOTIMG_ARGS += --pagesize $(BOARD_KERNEL_PAGESIZE)
 BOARD_MKBOOTIMG_ARGS += --ramdisk_offset $(BOARD_RAMDISK_OFFSET)

ARIPIN897/android-device-oppo6765

Signed-off-by: Aryan Sinha <sinha.aryan03@gmail.com>
Signed-off-by: KAGA-KOKO <kotakandis185@gmail.com>

BoardConfig.mk
@@ -78,6 +78,7 @@ TARGET_KERNEL_HEADER_ARCH := arm64
 TARGET_KERNEL_CONFIG := oppoa12_defconfig
 TARGET_KERNEL_SOURCE := kernel/oppo/CPH2083
 TARGET_KERNEL_CLANG_COMPILE := true
+TARGET_KERNEL_CLANG_VERSION := r316199
 
 # Recovery
 TARGET_RECOVERY_FSTAB := $(DEVICE_PATH)/rootdir/etc/fstab.mt6765